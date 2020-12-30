230755badcb0fe57ee153d853f7eb503886a6019
FilipeMCruz <filipecruz@tuta.io>
2020-12-30 16:25:32 +0000

    Update to startpage

--- current/startpage/.startpage/assets/js/script.js
+++ current/startpage/.startpage/assets/js/script.js
@@ -1,3 +1,13 @@
+function search(event) {
+    var x = event.key;
+    if (x == "Enter") {
+        event.preventDefault()
+        let text = document.getElementById("searchbar").value;
+        url = 'https://duckduckgo.com/?q=' + text
+        window.open(url, '_self');
+    }
+}
+
 function startTime() {
     const currentDate = new Date();
     document.getElementById("header-time").innerHTML = checkTime(currentDate.getHours()) + ":" + checkTime(currentDate.getMinutes());
